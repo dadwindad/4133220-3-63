5ba006ef62edb204c6bdd56ef5060f25f0af815a
dadwindad <vongpramate@gmail.com>
2021-05-03 11:20:57 +0700

    LAB10

---
+++ LAB10/models/del_member/db_del.php
@@ -0,0 +1,16 @@
+<?php
+require "./conDB.php";
+
+$id = $_GET['id'];
+
+try{
+    $SQL = "DELETE FROM tb_member 
+            WHERE member_id = '$id'
+            ";
+    $conn->query($SQL);
+}
+catch(Exception $e){
+    echo $e->getMessage();
+}
+?>
+
